01e44a63b3d3c54f80141f9cfbe422a41a910f10
Renzo-Espinola <renzo.espinola87@gmail.com>
2020-11-17 01:12:44 -0300

    ***PRY Qa-Service -se modifico el nombre del pry Consumer a Qa -se modifico bootstrap.yml

--- src/main/java/com/spring/consumerservice/QaServiceApplication.java
+++ src/main/java/com/spring/consumerservice/QaServiceApplication.java
@@ -2,26 +2,18 @@ package com.spring.consumerservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
 @SpringBootApplication
-public class ConsumerServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConsumerServiceApplication.class, args);
-    }
-}
-
 @RestController
-class Consumer {
+public class QaServiceApplication {
     @Value("$ {message}")
     private String message;
 
@@ -36,4 +28,9 @@ class Consumer {
 
         return response;
     }
-}
+
+        public static void main(String[] args) {
+            SpringApplication.run(QaServiceApplication.class, args);
+        }
+    }
+
